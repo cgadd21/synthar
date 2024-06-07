60a76da20fbc770105c94120de5e6fe71054a4d6
cgadd21 <ctg7866@g.rit.edu>
2024-06-06 23:51:50 -0400

    Update App component with new design and links

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,49 @@
 export const App = () => (
-  <div>
-    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  <div className="flex items-center justify-center min-h-screen">
+    <div className="space-y-10">
+      <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold [&amp;::selection]:text-base-content relative col-start-1 row-start-1 bg-[linear-gradient(90deg,theme(colors.error)_0%,theme(colors.secondary)_9%,theme(colors.secondary)_42%,theme(colors.primary)_47%,theme(colors.accent)_100%)] bg-clip-text [-webkit-text-fill-color:transparent] [&amp;::selection]:bg-blue-700/20 [@supports(color:oklch(0%_0_0))]:bg-[linear-gradient(90deg,oklch(var(--s))_4%,color-mix(in_oklch,oklch(var(--s)),oklch(var(--er)))_22%,oklch(var(--p))_45%,color-mix(in_oklch,oklch(var(--p)),oklch(var(--a)))_67%,oklch(var(--a))_100.2%)]">
+        Cooper is swening
+      </h1>
+      <div className="mockup-code glass">
+        <pre data-prefix="🪐">
+          <code>npm run synthar</code>
+        </pre>
+        <pre data-prefix="👨🏻‍💻">
+          <code>
+            <a href="https://github.com/cgadd21" target="_blank">
+              github
+            </a>
+          </code>
+        </pre>
+        <pre data-prefix="🖇️">
+          <code>
+            <a href="https://www.linkedin.com/in/coopergadd/" target="_blank">
+              linkedin
+            </a>
+          </code>
+        </pre>
+        <pre data-prefix="👨🏻‍💼">
+          <code>
+            <a href="https://ctgadd.com/luminify/" target="_blank">
+              luminify
+            </a>
+          </code>
+        </pre>
+        <pre data-prefix="🌥️">
+          <code>
+            <a href="https://ctgadd.com/zephyr/" target="_blank">
+              zephryr
+            </a>
+          </code>
+        </pre>
+        <pre data-prefix="🪵">
+          <code>
+            <a href="https://atticapackageco.com/" target="_blank">
+              attica package company
+            </a>
+          </code>
+        </pre>
+      </div>
+    </div>
   </div>
 );
