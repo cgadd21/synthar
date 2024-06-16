5a4198da4ab65cdc35ef7f02b84911dac5621f95
cgadd21 <ctg7866@g.rit.edu>
2024-06-15 23:40:28 -0400

    Removed heading and apc link

--- src/app.tsx
+++ src/app.tsx
@@ -1,42 +1,30 @@
 export const App = () => (
   <div className="flex min-h-screen items-center justify-center">
-    <div className="space-y-4 sm:space-y-10">
-      <h1 className="[&amp;::selection]:text-base-content [&amp;::selection]:bg-blue-700/20 bg-[linear-gradient(90deg,theme(colors.error)_0%,theme(colors.secondary)_9%,theme(colors.secondary)_42%,theme(colors.primary)_47%,theme(colors.accent)_100%)] bg-clip-text text-2xl font-bold [-webkit-text-fill-color:transparent] sm:text-3xl md:text-4xl lg:text-5xl [@supports(color:oklch(0%_0_0))]:bg-[linear-gradient(90deg,oklch(var(--s))_4%,color-mix(in_oklch,oklch(var(--s)),oklch(var(--er)))_22%,oklch(var(--p))_45%,color-mix(in_oklch,oklch(var(--p)),oklch(var(--a)))_67%,oklch(var(--a))_100.2%)]">
-        Cooper is swening
-      </h1>
-      <div className="glass mockup-code rounded">
-        <pre data-prefix="🪐">
-          <code>npm run synthar</code>
-        </pre>
-        <pre data-prefix="👨🏻‍💻">
-          <code>
-            <a href="https://github.com/cgadd21" target="_blank">
-              github
-            </a>
-          </code>
-        </pre>
-        <pre data-prefix="👨🏻‍💼">
-          <code>
-            <a href="https://ctgadd.com/luminify/" target="_blank">
-              luminify
-            </a>
-          </code>
-        </pre>
-        <pre data-prefix="🌥️">
-          <code>
-            <a href="https://ctgadd.com/zephyr/" target="_blank">
-              zephryr
-            </a>
-          </code>
-        </pre>
-        <pre data-prefix="🪵">
-          <code>
-            <a href="https://atticapackageco.com/" target="_blank">
-              attica package company
-            </a>
-          </code>
-        </pre>
-      </div>
+    <div className="glass mockup-code rounded">
+      <pre data-prefix="🪐">
+        <code>npm run synthar</code>
+      </pre>
+      <pre data-prefix="👨🏻‍💻">
+        <code>
+          <a href="https://github.com/cgadd21" target="_blank">
+            github
+          </a>
+        </code>
+      </pre>
+      <pre data-prefix="👨🏻‍💼">
+        <code>
+          <a href="https://ctgadd.com/luminify/" target="_blank">
+            luminify
+          </a>
+        </code>
+      </pre>
+      <pre data-prefix="🌥️">
+        <code>
+          <a href="https://ctgadd.com/zephyr/" target="_blank">
+            zephryr
+          </a>
+        </code>
+      </pre>
     </div>
   </div>
 );
