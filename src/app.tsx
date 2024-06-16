1430daafc9b96032e6a6e19e8350a3ded3915f8a
cgadd21 <ctg7866@g.rit.edu>
2024-06-15 23:32:55 -0400

    prettier tailwind config

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,10 @@
 export const App = () => (
-  <div className="flex items-center justify-center min-h-screen">
+  <div className="flex min-h-screen items-center justify-center">
     <div className="space-y-4 sm:space-y-10">
-      <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold [&amp;::selection]:text-base-content relative col-start-1 row-start-1 bg-[linear-gradient(90deg,theme(colors.error)_0%,theme(colors.secondary)_9%,theme(colors.secondary)_42%,theme(colors.primary)_47%,theme(colors.accent)_100%)] bg-clip-text [-webkit-text-fill-color:transparent] [&amp;::selection]:bg-blue-700/20 [@supports(color:oklch(0%_0_0))]:bg-[linear-gradient(90deg,oklch(var(--s))_4%,color-mix(in_oklch,oklch(var(--s)),oklch(var(--er)))_22%,oklch(var(--p))_45%,color-mix(in_oklch,oklch(var(--p)),oklch(var(--a)))_67%,oklch(var(--a))_100.2%)]">
+      <h1 className="[&amp;::selection]:text-base-content [&amp;::selection]:bg-blue-700/20 bg-[linear-gradient(90deg,theme(colors.error)_0%,theme(colors.secondary)_9%,theme(colors.secondary)_42%,theme(colors.primary)_47%,theme(colors.accent)_100%)] bg-clip-text text-2xl font-bold [-webkit-text-fill-color:transparent] sm:text-3xl md:text-4xl lg:text-5xl [@supports(color:oklch(0%_0_0))]:bg-[linear-gradient(90deg,oklch(var(--s))_4%,color-mix(in_oklch,oklch(var(--s)),oklch(var(--er)))_22%,oklch(var(--p))_45%,color-mix(in_oklch,oklch(var(--p)),oklch(var(--a)))_67%,oklch(var(--a))_100.2%)]">
         Cooper is swening
       </h1>
-      <div className="mockup-code glass rounded">
+      <div className="glass mockup-code rounded">
         <pre data-prefix="🪐">
           <code>npm run synthar</code>
         </pre>
